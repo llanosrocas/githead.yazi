476be6f06f5a3ccbd12f3932f3c8293434da1fa8
imsi32 <81227251+imsi32@users.noreply.github.com>
2024-07-27 11:37:35 +0300

    fix: Overriding Default Config
    issue: Overriding default config might cause issues if different plugins overrides it at the same time.
    fix: Using children_add function solves these issues.

--- init.lua
+++ init.lua
@@ -195,11 +195,8 @@ local function setup(_, options)
       untracked,
     }
   end
-
-  Header._left = {
-    { Header.cwd,     id = 1, order = 1000 },
-    { Header.githead, id = 2, order = 2000 },
-  }
+  
+  Header:children_add(Header.githead, 1234, Header.LEFT)
 end
 
 return { setup = setup }
